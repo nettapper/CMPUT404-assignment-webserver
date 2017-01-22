287be2f25871ac9fff5e29abc4ebd9d95c6a71fa
Conner D <connerdunn7399@gmail.com>
2017-01-22 11:51:04 -0700

    Ex printout of a request + my name in README

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-#  coding: utf-8 
+#  coding: utf-8
 import SocketServer
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,8 +27,18 @@ import SocketServer
 # try: curl -v -X GET http://127.0.0.1:8080/
 
 
+# Example Print out of this file (server.py)
+# Got a request of: GET / HTTP/1.1
+# Host: 127.0.0.1:8080
+# User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:50.0) Gecko/20100101 Firefox/50.0
+# Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+# Accept-Language: en-US,en;q=0.5
+# Accept-Encoding: gzip, deflate
+# Connection: keep-alive
+# Upgrade-Insecure-Requests: 1
+
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
